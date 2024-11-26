b1507a13211ec29b670e56cad824a283d26a0346
Witek <wiktorskid@wp.pl>
2024-11-26 20:23:35 +0100

    add vercel json

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  // output: 'server',
-  // adapter: vercel()
+  integrations: [react(), tailwind(), vercel()],
+  output: "server",
+  adapter: vercel(),
 });
