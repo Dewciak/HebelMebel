5dacd6ce86572a6a51fc527ba4ddc2546fd6807d
Witek <wiktorskid@wp.pl>
2024-12-25 13:20:03 +0100

    test build delete vercel folder

--- .vercel/output/static/_astro/Navbar.JQ8nmvrE.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"./Navbar.DGtosV4A.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.rE2EVDCa.js";import"./index.zkFWU35Z.js";import"./StateStore.OUtCLXMS.js";export{e as default};
--- .vercel/output/static/_astro/index.CEF66xdE.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./index.rE2EVDCa.js";import{r as o}from"./index.zkFWU35Z.js";var t=o();const m=r(t);export{m as R,t as r};
