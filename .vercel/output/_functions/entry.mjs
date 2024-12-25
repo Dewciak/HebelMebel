c1d46bbac031cb465ce35286a85c0949766c207c
Witek <wiktorskid@wp.pl>
2024-12-25 13:13:23 +0100

    deploy test

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_C-pIrjvh.mjs';
-import { manifest } from './manifest_DneZbIrJ.mjs';
+import { manifest } from './manifest_IBPdBR0A.mjs';
 
 const serverIslandMap = new Map([
 ]);;
@@ -27,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "f169945a-5b67-4f66-8035-83c4fe61081e",
+    "middlewareSecret": "3a7a342b-1169-4916-95c2-128024c60c92",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
