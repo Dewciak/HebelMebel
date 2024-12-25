0966371de44c5a7439a4342aa23564ec814a0445
Witek <wiktorskid@wp.pl>
2024-12-25 13:05:30 +0100

    deploy test, deleted cache, installed deps again

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
-
 import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercelServerless(),
+  adapter: vercelServerless({
+    webAnalytics: {
+      enabled: false,
+    },
+    maxDuration: 8,
+  }),
 });
