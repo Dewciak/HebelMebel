47653c9e789fa94a20a253da081c01f40453988e
Witek <wiktorskid@wp.pl>
2025-06-03 20:20:58 +0200

    Add new about text

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -20,17 +20,20 @@ export function AboutDialog() {
           <DialogDescription></DialogDescription>
         </DialogHeader>
         <p className='text-justify dark:text-white overflow-y-scroll h-[400px] px-4 '>
-          Hebel Mebel to firma stolarska, która łączy tradycyjne rzemiosło z nowoczesnym designem. Od lat tworzymy
-          wyjątkowe meble na wymiar, które łączą funkcjonalność, trwałość i estetykę. Nasza pasja do drewna i dbałość o
-          detale sprawiają, że każdy projekt jest dopracowany w najmniejszym szczególe. Nasza misja to wnoszenie piękna
-          do Twojego domu poprzez meble, które są nie tylko praktyczne, ale także odzwierciedlają Twoje potrzeby i styl
-          życia. Pracujemy z najlepszymi materiałami i nowoczesnymi technologiami, ale zawsze z poszanowaniem
-          tradycyjnych technik stolarskich. Dlaczego warto wybrać Hebel Mebel? Indywidualne podejście – każdy projekt
-          dopasowujemy do Twoich oczekiwań. Jakość – tworzymy meble, które przetrwają pokolenia. Pasja – kochamy drewno
-          i jego naturalne piękno. Realizujemy projekty zarówno dla klientów indywidualnych, jak i biznesowych. Od
-          kuchni, przez garderoby, po meble biurowe – z nami każde wnętrze zyska niepowtarzalny charakter. Zapraszamy do
-          współpracy i wspólnego tworzenia mebli, które będą świadectwem naszej pasji i Twojego stylu! Hebel Mebel –
-          drewno w najlepszym wydaniu.
+          Hebel Mebel – meble z duszą, tworzone z serca, z prawdziwego drewna.
+          <br />
+          <br />
+          Jesteśmy rodzinną pracownią stolarską – ojciec i syn, którzy razem tworzą coś wyjątkowego. W Hebel Mebel
+          łączymy tradycyjne rzemiosło z prawdziwą pasją do litego drewna. <br />
+          <br /> Nasze meble to nie tylko ładne przedmioty – to rzeczy, które przetrwają lata i pokolenia. Każdy mebel
+          to dla nas osobna historia – zaczyna się od kawałka dębu, buku czy jesionu, a kończy w Twoim domu, nadając mu
+          niepowtarzalny charakter. <br />
+          <br />
+          Każdy etap powstawania mebla – od koncepcji, przez ręczną pracę, aż po finalne wykończenie – wykonujemy sami,
+          z troską o każdy kawałek drewna i sercem do pracy. Działamy głównie w Małopolsce, gdzie mieści się nasza
+          pracownia i realizujemy większość zamówień. <br />
+          Choć lokalni, nasze meble z radością trafiają do domów w całej Polsce – prosto z naszego serca do Twojego
+          wnętrza.
         </p>
       </DialogContent>
     </Dialog>
